31f129cb6f5c6d0f11b9ad00737b2fce5c446bf7
sarahanngwynn <93155807+sarahanngwynn@users.noreply.github.com>
2021-12-13 16:17:26 -0700

    added searchbar and useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Sarah Gwynn</footer>
+      </div>
     </div>
   );
 }
